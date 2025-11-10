1f9668804ba70f7822c1549abc086b86f4d62582
Arthur Mazzardo Naue <133718057+ArthurNaue@users.noreply.github.com>
2025-11-10 20:26:31 -0300

    File redirect

--- src/main/java/com/RockCafe/configuration/WebConfig.java
+++ src/main/java/com/RockCafe/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer
     @Override
     public void addViewControllers(ViewControllerRegistry registry) 
     {
-        registry.addRedirectViewController("/", "/api");
+        registry.addRedirectViewController("/", "/api/list_items");
     }
 
     @Override
